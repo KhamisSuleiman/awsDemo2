7f511d4d62232ee798762c63cc41c15a77e42bb2
KhamisSuleiman <33213724+KhamisSuleiman@users.noreply.github.com>
2023-09-04 15:28:34 +0300

    Add files via upload

---
+++ process.php
@@ -0,0 +1,30 @@
+<?php
+// Get form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+// Create a connection to your RDS database
+$servername = "mydb.c2wzbzgqfrv5.us-east-1.rds.amazonaws.com";
+$username = "khamis";
+$password = "kala1994";
+$dbname = "mydb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Insert data into the database
+$sql = "INSERT INTO user_data (name, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data stored successfully.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
